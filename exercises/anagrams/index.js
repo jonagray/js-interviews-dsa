66db8d63631426f87c330396fd94dad55a40d8d2
Jonny Graybill <jon.a.graybill@gmail.com>
2020-07-20 15:10:42 -0700

    initial challenge for anagrams complete

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,34 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// First iteration
+// .replace(/[^\w]/g, "")
+
+  // function anagrams(stringA, stringB) {
+  //   const aCharMap = buildCharMap(stringA);
+  //   const bCharMap = buildCharMap(stringB);
+  //   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+  //     return false;
+  //   };
+  //   for (let char in aCharMap) {
+  //     if (aCharMap[char] !== bCharMap[char]) {
+  //       return false;
+  //     };
+  //   };
+  //   return true;
+  // };
+
+// Helper function to turn string into character map
+  // function buildCharMap(str) {
+  //   const charMap = {};
+  //   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+  //     charMap[char] = charMap[char] + 1 || 1;
+  //   };
+  //   return charMap;
+  // }
+
+  function anagrams(stringA, stringB) {
+
+  };
 
 module.exports = anagrams;
