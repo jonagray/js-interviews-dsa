5981385803e7b0e72b6f447831b4cfe6d06a1f05
Jonny Graybill <jon.a.graybill@gmail.com>
2020-07-28 15:56:50 -0700

    finished pyramid solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// How is (n) related to the number of columns??
+  // Times (n) by itself, and then minus 1
+
+// Iterative Solution
+  // function pyramid(n) {
+  //   const midpoint = Math.floor((2 * n - 1) / 2);
+  //   for (let row = 0; row < n; row++) {
+  //     let level = '';
+  //     for (let column = 0; column < 2 * n - 1; column++) {
+  //       if (midpoint - row <= column && midpoint + row >= column) {
+  //         level += '#';
+  //       } else {
+  //         level += ' ';
+  //       }
+  //     }
+  //     console.log(level);
+  //   }
+  // };
+
+// Recursive Solution
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
+};
 
 module.exports = pyramid;
