e827b75e17789de76c03b8beb2a13c0472baae23
Jonny Graybill <jon.a.graybill@gmail.com>
2020-07-29 15:28:25 -0700

    finished find vowels challenge

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,44 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// First try (doesn't work)
+  // function vowels(str) {
+  //   // Make a character map
+  //   let result = 0;
+  //   let strMap = buildCharMap(str);
+  //   for (let char in strMap) {
+  //     if (Object.keys === 'a' || 'e' || 'o' || 'i' || 'u') {
+  //       result += strMap[char];
+  //     }
+  //   };
+  //   return result;
+  // };
+
+// Iterative Solution
+  // function buildCharMap (str) {
+  //   const charMap = {};
+  //   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+  //     charMap[char] = charMap[char] + 1 || 1;
+  //   };
+  //   return charMap;
+  // }
+
+  // function vowels(str) {
+  //   let counter = 0;
+  //   const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  //   for (let char of str.toLowerCase()) {
+  //     if (checker.includes(char)) {
+  //       counter++;
+  //     }
+  //   }
+  //   return counter;
+  // };
+
+// Cheater Solution
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+};
 
 module.exports = vowels;
