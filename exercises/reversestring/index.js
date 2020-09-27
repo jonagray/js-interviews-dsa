2a96c9ec01c4e252ea900d274f5a797bcfcc476e
Jonny Graybill <jon.a.graybill@gmail.com>
2020-09-27 12:08:35 -0700

    solved reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
+// Cheater methods
+  // function reverse(str) {
+  //   const arr = str.split('');
+  //   arr.reverse();
+  //   return arr.join('');
+  // }
 
-}
+  // function reverse(str) {
+  //   return str.split('').reverse().join('');
+  // };
+
+// Array helper method
+  // function reverse(str) {
+  //   return str.split('').reduce((reversed, character) => character + reversed, '');
+  // }
+
+// Iterative solution
+function reverse(str) {
+  // Create empty string called 'reversed'
+  // For each character in the string, take the character and add it to the start of the str
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed;
+  };
+  return reversed;
+};
 
 module.exports = reverse;
